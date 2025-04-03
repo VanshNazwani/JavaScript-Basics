f571b130714eee6f6c4a5dc566b94e639dedbcd2
VanshNazwani <nazwaniv@gmail.com>
2025-04-03 18:01:36 +0000

    JSON Basics

---
+++ JSON.js
@@ -0,0 +1,19 @@
+const course = {
+    name : "javascript",
+    price : "1000",
+    instructor : "hitesh"
+
+}
+
+const{price : p} = course // restructure any object
+
+console.log(p);
+
+
+// ** API ** 
+{
+    "name" : "vansh",
+    "coursename" : "javascript",
+    "price" : "5000",
+
+}
