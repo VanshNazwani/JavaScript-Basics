c360b15682fc2c770c14107235e369378ecdbc29
VanshNazwani <nazwaniv@gmail.com>
2025-04-19 21:45:52 +0000

    ...

---
+++ CLASSES_OBJECT/iterate.js
@@ -0,0 +1,27 @@
+const descriptor = Object.getOwnPropertyDescriptor(Math, "PI");
+console.log(descriptor);
+
+const chai = {
+    name: "ginger chai",
+    price: "250",
+    isavailable: true,
+    orderchai: function () {
+        console.log("chai not prepared");
+    }
+};
+
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+Object.defineProperty(chai, 'name', {
+    writable: false,
+    enumerable: false,
+});
+
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+// 🔁 Correct way to iterate over an object
+for (const [key, value] of Object.entries(chai)) {
+    if (typeof value !== 'function') {
+        console.log(`key : ${key} : ${value}`);
+    }
+}
